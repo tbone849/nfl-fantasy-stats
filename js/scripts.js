9b2b294c11bb8275b9c72f65a00e598590def9eb
Tim Sarvey <timsarvey@gmail.com>
2015-09-29 12:13:22 -0400

    Change team abbreviation array into an object

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,40 @@
 $( document ).ready(function() {
-    var teamAbbr = ["ARI", "ATL", "BAL", "BUF", "CAR", "CHI", "CIN", "CLE", "DAL", 
-    				  "DEN", "DET", "GB", "TEN", "HOU", "IND", "JAX", "KC", "STL", 
-    				  "OAK", "MIA", "MIN", "NE", "NO", "NYG", "NYJ", "PHI", "PIT", 
-    				  "SD", "SF", "SEA", "TB", "WAS"];
-    for(var t = 0; t < teamAbbr.length; t++){
-    	$('#teamNames').append('<option value="' + teamAbbr[t] + '">' + teamAbbr[t] + '</option>');
+    var teams = {
+    	"ARI": "Arizona Cardinals", 
+    	"ATL": "Atlanta Falcons",
+    	"BAL": "Baltimore Ravens", 
+    	"BUF": "Buffalo Bills", 
+    	"CAR": "Carolina Panthers", 
+    	"CHI": "Chicago Bears", 
+    	"CIN": "Cincinnati Bengals", 
+    	"CLE": "Cleveland Browns", 
+    	"DAL": "Dallas Cowboys",
+    	"DEN": "Denver Broncos", 
+    	"DET": "Detroit Lions", 
+    	"GB":  "Green Bay Packers", 
+    	"TEN": "Tennessee Titans", 
+    	"HOU": "Houston Texans", 
+    	"IND": "Indianapolis Colts", 
+    	"JAX": "Jacksonville Jaguars", 
+    	"KC":  "Kansas City Chiefs", 
+    	"STL": "St. Louis Rams", 
+    	"OAK": "Oakland Raiders", 
+    	"MIA": "Miami Dolphins", 
+    	"MIN": "Minnesota Vikings", 
+    	"NE":  "New England Patriots", 
+    	"NO":  "New Orleans Saints", 
+    	"NYG": "New York Giants", 
+    	"NYJ": "New York Jets", 
+    	"PHI": "Philadelphia Eagles", 
+    	"PIT": "Pittsburgh Steelers", 
+    	"SD":  "San Diego Chargers", 
+    	"SF":  "San Fransisco 49ers", 
+    	"SEA": "Seattle Seahawks", 
+    	"TB": "Tampa Bay Buccaneers", 
+    	"WAS": "Washington Redskins"
+	};
+
+    for(t in teams){
+    	$('#teamNames').append('<option value="' + t + '">' + t + '</option>');
     }
 });
